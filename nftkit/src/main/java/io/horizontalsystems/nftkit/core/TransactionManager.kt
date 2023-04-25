bf4fa20731d0e6d0323f45dd45c6e54bf5063130
Aaron Thomas <aaronthomas2000@hotmail.com>
2023-04-25 21:39:45 +0100

    Update TransactionManager.kt
    Update to and value params

--- nftkit/src/main/java/io/horizontalsystems/nftkit/core/TransactionManager.kt
+++ nftkit/src/main/java/io/horizontalsystems/nftkit/core/TransactionManager.kt
@@ -12,15 +12,15 @@ class TransactionManager(ethereumKit: EthereumKit) {
 
     fun transferEip721TransactionData(contractAddress: Address, to: Address, tokenId: BigInteger) =
         TransactionData(
-            to = contractAddress,
-            value = BigInteger.ZERO,
+            to = to,
+            value = value,
             input = Eip721SafeTransferFromMethod(address, to, tokenId, byteArrayOf()).encodedABI()
         )
 
     fun transferEip1155TransactionData(contractAddress: Address, to: Address, tokenId: BigInteger, value: BigInteger) =
         TransactionData(
-            to = contractAddress,
-            value = BigInteger.ZERO,
+            to = to,
+            value = value,
             input = Eip1155SafeTransferFromMethod(address, to, tokenId, value, byteArrayOf()).encodedABI()
         )
 }
